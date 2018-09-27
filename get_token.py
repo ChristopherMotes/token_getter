b8dbd548a7af6e80f5de74c0a2b720b57e4a7c70
Christopher Motes <christopher@flux7.com>
2018-09-27 11:55:59 -0500

    tons of checking missing because  I had sensitive values during testing.

--- get_token.py
+++ get_token.py
@@ -1,16 +1,40 @@
 #!/usr/bin/python
 import boto3
-import json
-import sys
+import ConfigParser
 
-def get_code():
-    return_value = raw_input("enter security token: ")
-    return return_value
-    
 def get_creds(mfaCode):
+    config = ConfigParser.SafeConfigParser()
+    config.read('account.cfg')
+    configDict = dict(config.items('default'))
     client = boto3.client('sts')    
+    response = client.assume_role(
+        RoleArn=configDict['rolearn'],
+        RoleSessionName='ScriptGenreated',
+        SerialNumber=configDict['serialnumber'],
+        TokenCode=mfaCode
+    )
+    return response
 
+def config_file_updater(responseDict):
+    config = ConfigParser.SafeConfigParser()
+    awsFile = "/home/motes/.aws/credentials"
+    config.read(awsFile)
+    try: 
+        config.add_section('swithRoleRoot')
+    except ConfigParser.DuplicateSectionError:
+        pass
+    except:
+        raise
+    config.set('swithRoleRoot', 'output', 'text')
+    config.set('swithRoleRoot', 'region', 'us-east-2')
+    config.set('swithRoleRoot', 'aws_access_key_id', responseDict['Credentials']['AccessKeyId'])
+    config.set('swithRoleRoot', 'aws_secret_access_key', responseDict['Credentials']['SecretAccessKey'])
+    config.set('swithRoleRoot', 'aws_session_token', responseDict['Credentials']['SessionToken'])
+
+    with open(awsFile, 'wb') as configfile:
+        config.write(configfile)
+  
 if __name__ == "__main__":
-    mfa_code = get_code()
-    get_creds(mfa_code)
+    mfa_code = raw_input("enter security token: ")
+    config_file_updater(get_creds(mfa_code))
    
